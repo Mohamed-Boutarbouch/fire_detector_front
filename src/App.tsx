38844587822fbb3433e4e55188dd3723a51c7664
Mohamed Boutarbouch <mohamedboutarbouch48@gmail.com>
2024-05-18 18:27:28 +0100

    Feat: Query the supabase database

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { Icon } from "leaflet";
-
 import "leaflet/dist/leaflet.css";
 import fireIcon from "./assets/fire.png";
 import { supabase } from "./supabaseClient";
 import { useMap } from "react-leaflet";
 
-// const socket = io("http://localhost:5001");
+interface Camera {
+  id: number;
+  latitude: string;
+  longitude: string;
+  area_id: number;
+  created_at: string;
+}
 
 export function App() {
-
   const map = useMap();
 
-  map.on('click', function(e) {
+  map.on("click", function (e) {
     // Get the coordinates of the click event
     const coords = e.latlng;
     console.log("Coordinates: " + coords.lat + ", " + coords.lng);
-});
+  });
 
-  
   const [
     endPoint,
     // setEndpoint
@@ -42,20 +45,19 @@ export function App() {
   // }, []);
 
   useEffect(() => {
-    const realtime = supabase.realtime;
+    async function fetchCameras() {
+      const { data, error } = await supabase.from("cameras").select("*");
 
-    // Subscribe to a channel
-    const channel = realtime.channel("my-channel");
+      console.log(data);
 
-    // Listen for new messages
-    // channel.on("INSERT", (payload) => {
-    //   console.log("New message:", payload);
-    // });
+      if (error) {
+        console.error("Error fetching cameras:", error.message);
+        return [];
+      }
+      return data;
+    }
 
-    // Unsubscribe when the component unmounts
-    return () => {
-      channel.unsubscribe();
-    };
+    fetchCameras();
   }, []);
 
   const customIcon = new Icon({
@@ -67,24 +69,25 @@ export function App() {
     <div style={{ height: "100vh", width: "100vw" }}>
       <MapContainer
         style={{ height: "100%", width: "100%" }}
-        center={endPoint ? endPoint : [33.98460250512071, -5.019231838515444]}
+        center={endPoint || [33.98460250512071, -5.019231838515444]}
         zoom={15}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {endPoint && (
-          <Marker position={endPoint} icon={customIcon}>
-            <Popup>
-              {imageUrl ? (
-                <img src={imageUrl} alt="Fire" style={{ width: "200px" }} />
-              ) : (
-                "Fire detected"
-              )}
-            </Popup>
+        {cameras.map((camera) => (
+          <Marker
+            key={camera.id}
+            position={[
+              parseFloat(camera.latitude),
+              parseFloat(camera.longitude),
+            ]}
+            icon={customIcon}
+          >
+            <Popup>{camera.area_id}</Popup>
           </Marker>
-        )}
+        ))}
       </MapContainer>
     </div>
   );
